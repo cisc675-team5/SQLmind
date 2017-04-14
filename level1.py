11c9dba52ab6c3ef147e8767ae0bd9106ed6c561
Shouzhi Huang <Frank@wifi-roaming-128-4-213-172.host.udel.edu>
2017-04-13 21:45:27 -0400

    Fully implement the SQLprocessor
    Output is a list of key value pair, the key is parent function and the
    value is child function.

--- level1.py
+++ level1.py
@@ -1,75 +1,116 @@
 import sys
 import re
+from sets import Set
 
 
-Vertexes = []
-Store = []
-sqlFileName = sys.argv[1]
-
-
-
-def processInnerfunction(ends): #extract the function name if it's in a block
-	global Vertexes
-	if len(ends)==0:
-		return
-	for e in ends:
-		processBlock(e)
+class SQLProcessor:
 
 
 
-def processBlock(block): #extract function name in block
+	def __init__(self,sqlFileName):
+		self.__dict__ = {}
+		self.__Store = []
+		self.__result = Set()
+		self.__functions = Set()
+		self.__processSQLFile(sqlFileName)
+		self.__resultList = []
+		self.__removeInvalidNode()
 
-	global Vertexes
-	global Store 
-	if len(block)<2:
-		return
-	for i in range(0,len(block)):
-		if block[i]=='(':
-			Vertexes.append(tuple([Store[-1],block[:i]]))
-			Store.append(block[:i])
-			processInnerfunction(block[i+1:-1].split(','))
-			Store.pop();
+
+
+
+	def __processInnerfunction(self,ends): #extract the function name if it's in a block
+		if len(ends)==0:
 			return
+		for e in ends:
+			self.__processBlock(e)
 
 
 
-def processLine(line): #split line by operators or semicolon
-	blocks=filter(None, re.split("[ \-\+\=\/\;]+", line))
-	if len(blocks)==0:
-		return
-	if blocks[0]=='FUNCTION' or blocks[0]=='function':
-		Store.append(blocks[1])
-		return
-	for block in blocks:
-		processBlock(block)
+
+	def __processBlock(self,block): #extract function name in block
+		if len(block)<2:
+			return
+		for i in range(0,len(block)):
+			if block[i]=='(':
+				self.__result.add(tuple([self.__Store[-1],block[:i]]))
+				self.__Store.append(block[:i])
+				self.__processInnerfunction(block[i+1:-1].split(','))
+				self.__Store.pop()
+				return
 
 
-def processSQLfunction(function): #split function by '\n'
-	global Store
-	lines = function.split('\n')
-	for line in lines:
-		if len(line)>0:
-			processLine(line)
 
 
-				
-def processSQLFile(sqlFileName): #read in the file and split the file by 'CREATE'
-	fd = open(sqlFileName, 'r')
-	sqlFile = fd.read()
-	fd.close()
-	sqlFuntions = re.split('CREATE',sqlFile)
-	for function in sqlFuntions:
-		processSQLfunction(function)
+	def __processLine(self,line): #split line by operators or semicolon
+		blocks=filter(None, re.split("[ \-\+\=\/\;]+", line))
+		if len(blocks)==0:
+			return
+		if blocks[0]=='FUNCTION' or blocks[0]=='function':
+			self.__Store.append(blocks[1])
+			return
+		for block in blocks:
+			self.__processBlock(block)
+
+
+
+
+	def __processSQLfunction(self,function): #split function by '\n'
+		lines = function.split('\n')
+		for line in lines:
+			if len(line)>0:
+				self.__processLine(line)
+
+
+
+
+	def __processSQLFile(self,sqlFileName): #read in the file and split the file by 'CREATE'
+		fd = open(sqlFileName, 'r')
+		sqlFile = fd.read()
+		fd.close()
+		sqlFuntions = re.split('CREATE',sqlFile)
+		self.__extractDefinedSQLFunction(sqlFuntions)
+		for function in sqlFuntions:
+			self.__processSQLfunction(function)
+
+
+
+
+	def __extractDefinedSQLFunction(self,sqlFunctions):
+		for function in sqlFunctions:
+			self.__getNameOfFunction(function)
+
+
+
+
+	def __getNameOfFunction(self,function):
+		lines = function.split('\n')
+		if len(lines)>0:
+			line = lines[0]
+			blocks=filter(None, re.split("[ \-\+\=\/\;]+", line))
+			if len(blocks)>0:
+				self.__functions.add(blocks[1])
+
+
+	def __removeInvalidNode(self):
+		for e in self.__result:
+			parent = e[0]
+			child = e[1]			
+			if (parent in self.__functions) and (child in self.__functions):
+				self.__resultList.append(e)
+
+
+
+	def getResult(self):
+		return self.__resultList
+
+
+
 
 def main():
-	global sqlFileName
-	global Vertexes
-	processSQLFile(sqlFileName)
-	print Vertexes
-
-if __name__ == "__main__":
-    main()
+	processor = SQLProcessor(sys.argv[1])
+	print processor.getResult()
 
 
-
-
+if __name__=='__main__':
+	main()
--- test.sql
+++ test.sql
@@ -14,4 +14,21 @@ BEGIN
   GROUP BY soh.TaxAmt, soh.Freight ;
   RETURN @SalesOrderTotal ;
 END;
+
+CREATE FUNCTION ndjsbdjs (@SalesOrderID INT)
+RETURNS MONEY
+WITH SCHEMABINDING AS
+BEGIN
+  DECLARE @SalesOrderTotal AS MONEY ;
+  SELECT  @SalesOrderTotal = 
+            SUM(sod.LineTotal,sun(wnwn(suns())))+sum(SUM()) 
+            + soh.TaxAmt 
+            + soh.Freight
+  FROM    Sales.SalesOrderHeader AS soh
+          INNER JOIN Sales.SalesOrderDetail AS sod
+            ON soh.SalesOrderID = sod.SalesOrderID
+  WHERE   soh.SalesOrderID = @SalesOrderId
+  GROUP BY soh.TaxAmt, soh.Freight ;
+  RETURN @SalesOrderTotal ;
+END;
 GO
